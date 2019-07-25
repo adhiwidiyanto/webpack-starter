32b7fa026c706071285cefbc3a3841936a98eb0c
adhi <adhi.widiyanto@idntimes.com>
2019-07-25 17:09:39 +0700

    add purgecss to cleaning unsused css style

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
+const purgeCss = require('@fullhuman/postcss-purgecss')({
+
+    content: [
+        './src/**/*.scss',
+        './src/**/*.js',
+        './*.html',
+    ],
+
+    defaultExtrator: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
 module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
+        ...process.env.NODE_ENV === 'production' ? [purgeCss] : []
     ],
-  }
+}
